271624d6fab13f760b9623a8c63b8d3bbf173f2e
fabiosoares-silva <fabiosores.soares@gmail.com>
2023-12-07 16:54:38 -0300

    Implementar classes e concluir projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "", modelo: "", imei: "", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone(numero: "", modelo: "", imei: "", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
